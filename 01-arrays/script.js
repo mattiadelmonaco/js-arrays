86015181efea903cfa8e9ae82c47a9a2a41bdcca
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-02 16:22:35 +0100

    feat: add exercise 3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,7 +18,9 @@ teachers[4] = "Patrick"
 console.log (teachers[4])
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log (teachers)
+console.log (lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
