0df8e84b33940793757245cd23093bcff44849f2
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-02 18:03:44 +0100

    feat: add exercise bonus 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,6 +15,15 @@ console.log (reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = []
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.unshift(teachers[i])
+  }
+}
+
+console.log (longNames)
+  
 
 // 3. Rimuovi 'Ed' dall'array teachers
