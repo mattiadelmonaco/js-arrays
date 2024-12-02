2d3207a761c48d16bfb28e34766c4538a551cf9a
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-02 16:34:09 +0100

    feat: add exercise 6 and 7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -32,10 +32,14 @@ teachers.push("Vanessa")
 console.log (teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah")
+console.log (teachers)
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes("Fabio");
+console.log ("Fabio è presente?")
+console.log (isFabioPresent)
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
